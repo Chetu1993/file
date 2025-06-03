fa43486f0783d344c8c8d5de024b8d11b072de6f
chetu1993 <schetankumar123@gmail.com>
2025-06-03 14:56:29 +0530

    modified

---
+++ good.py
@@ -0,0 +1,67 @@
+
+
+import streamlit as st
+
+st.title("welcome to streamlit server")
+name=st.text_input("enter your name:")
+company=st.text_input("enter your company name:")
+email=st.text_input("enter your email:")
+phone_no=st.text_input("enter your phone number:")
+
+
+st.write(f"your name is: {name}")
+st.write(f"your company name is: {company}")
+st.write(f"your email is: {name}")
+st.write(f"your phone number is: {phone_no}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
